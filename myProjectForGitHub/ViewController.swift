dc8f2dce0b5808956cd2ba4b7a25c6874d04d76a
Jessi Chartier <jessi@mobilemakers.co>
2017-06-01 12:12:15 -0500

    Initial Commit

---
+++ myProjectForGitHub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myProjectForGitHub
+//
+//  Created by Jessi Chartier on 6/1/17.
+//  Copyright © 2017 Jessi Chartier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
